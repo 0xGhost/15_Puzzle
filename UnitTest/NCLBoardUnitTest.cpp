548645bf9ff172c0ef2121c779cb4ef3e3056d6c
Yiang Lu <xxlyaxx@163.com>
2019-10-13 23:29:27 +0100

    refine UnitTest for NCLBoard: more test for method CheckContinuous()

--- UnitTest/NCLBoardUnitTest.cpp
+++ UnitTest/NCLBoardUnitTest.cpp
@@ -56,5 +56,105 @@ namespace UnitTest
 			delete result0;
 			result0 = nullptr;
 		}
+
+		TEST_METHOD(CheckRowReverseContinuous)
+		{
+			int data[16] =
+			{ 4, 3, 2, 1,
+			5, 6, 17, 8,
+			9, 10, 16, 12,
+			15, 14, 13, -1 };
+
+			NCLBoard board1(4, data);
+			ContinuousNumber* result0 = board1.CheckContinuous();
+			ContinuousNumber result1;
+			result1.column = 0;
+			result1.row = 0;
+			result1.columnReverse = 0;
+			result1.rowReverse = 2;
+			Assert::IsTrue(*result0 == result1);
+			delete result0;
+			result0 = nullptr;
+		}
+
+		TEST_METHOD(CheckColumnContinuous)
+		{
+			int data[16] =
+			{ 1, 5, 9, 17,
+			2, 6, 7, 18,
+			3, 10, 20, 19,
+			4, 14, 15, -1 };
+
+			NCLBoard board1(4, data);
+			ContinuousNumber* result0 = board1.CheckContinuous();
+			ContinuousNumber result1;
+			result1.column = 2;
+			result1.row = 0;
+			result1.columnReverse = 0;
+			result1.rowReverse = 0;
+			Assert::IsTrue(*result0 == result1);
+			delete result0;
+			result0 = nullptr;
+		}
+
+		TEST_METHOD(CheckColumnReverseContinuous)
+		{
+			int data[16] =
+			{ 4, 5, 9, 19,
+			3, 6, 7, 18,
+			2, 10, 20, 17,
+			1, 14, 15, -1 };
+
+			NCLBoard board1(4, data);
+			ContinuousNumber* result0 = board1.CheckContinuous();
+			ContinuousNumber result1;
+			result1.column = 0;
+			result1.row = 0;
+			result1.columnReverse = 2;
+			result1.rowReverse = 0;
+			Assert::IsTrue(*result0 == result1);
+			delete result0;
+			result0 = nullptr;
+		}
+
+		TEST_METHOD(CheckMixedContinuous1)
+		{
+			int data[16] =
+			{ 4, 5, 9, 17,
+			3, 6, 7, 18,
+			2, 10, 20, 19,
+			1, 14, 15, -1 };
+
+			NCLBoard board1(4, data);
+			ContinuousNumber* result0 = board1.CheckContinuous();
+			ContinuousNumber result1;
+			result1.column = 1;
+			result1.row = 0;
+			result1.columnReverse = 1;
+			result1.rowReverse = 0;
+			Assert::IsTrue(*result0 == result1);
+			delete result0;
+			result0 = nullptr;
+		}
+
+		TEST_METHOD(CheckMixedContinuous2)
+		{
+			int data[16] =
+			{ 1, 2, 3, 4,
+			5, 6, 7, 8,
+			20, 19, 18, 17,
+			13, 14, 15, -1 };
+
+			NCLBoard board1(4, data);
+			ContinuousNumber* result0 = board1.CheckContinuous();
+			ContinuousNumber result1;
+			result1.column = 0;
+			result1.row = 3;
+			result1.columnReverse = 0;
+			result1.rowReverse = 1;
+			Assert::IsTrue(*result0 == result1);
+			delete result0;
+			result0 = nullptr;
+		}
 	};
 }
